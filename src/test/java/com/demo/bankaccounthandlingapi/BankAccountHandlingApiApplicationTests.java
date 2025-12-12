291a3c63ca5d5951c230d4913819f67c05dab513
Maiko Metsalu <maiko.metsalu@ridango.com>
2025-12-12 20:06:20 +0200

    Initial project setup

---
+++ src/test/java/com/demo/bankaccounthandlingapi/BankAccountHandlingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.bankaccounthandlingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountHandlingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
